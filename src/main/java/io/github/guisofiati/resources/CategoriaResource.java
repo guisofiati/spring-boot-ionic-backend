a42c5b3b9b97c870ae93a3ba6624afe1963d1850
Sofiati <gui_sf@hotmail.com>
2020-09-11 20:21:50 -0300

    Ajustes no endpoint - /categorias/{id} -

--- src/main/java/io/github/guisofiati/resources/CategoriaResource.java
+++ src/main/java/io/github/guisofiati/resources/CategoriaResource.java
@@ -18,10 +18,8 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
+	public ResponseEntity<?> find(@PathVariable Integer id) {		
 		Categoria obj = service.find(id);
-		return ResponseEntity.ok().body(obj);
-		
-	}
+		return ResponseEntity.ok().body(obj);		
+	}	
 }
--- src/main/java/io/github/guisofiati/services/CategoriaService.java
+++ src/main/java/io/github/guisofiati/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import io.github.guisofiati.domain.Categoria;
 import io.github.guisofiati.repositories.CategoriaRepository;
+import io.github.guisofiati.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " +id+ ", Tipo: " +Categoria.class.getName()));
 		} 
 }
