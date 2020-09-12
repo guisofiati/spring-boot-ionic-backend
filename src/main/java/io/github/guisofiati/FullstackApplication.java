304350023610f296896023052d477d60918843a1
Sofiati <gui_sf@hotmail.com>
2020-09-12 16:48:17 -0300

    Cliente, Telefone e Endereco

--- src/main/java/io/github/guisofiati/FullstackApplication.java
+++ src/main/java/io/github/guisofiati/FullstackApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.guisofiati.domain.Categoria;
 import io.github.guisofiati.domain.Cidade;
+import io.github.guisofiati.domain.Cliente;
+import io.github.guisofiati.domain.Endereco;
 import io.github.guisofiati.domain.Estado;
 import io.github.guisofiati.domain.Produto;
+import io.github.guisofiati.domain.enums.TipoCliente;
 import io.github.guisofiati.repositories.CategoriaRepository;
 import io.github.guisofiati.repositories.CidadeRepository;
+import io.github.guisofiati.repositories.ClienteRepository;
+import io.github.guisofiati.repositories.EnderecoRepository;
 import io.github.guisofiati.repositories.EstadoRepository;
 import io.github.guisofiati.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class FullstackApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApplication.class, args);
@@ -64,6 +73,26 @@ public class FullstackApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva",
+				"maria@gmail.com",
+				"36378912377",
+				TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", 
+				"300", "Apto 303", "Jardim", "38220834", 
+				cli1, c1);
+		
+		Endereco e2 = new Endereco(null, "Avenida Matos", 
+				"105", "Sala 800", "Centro", "38777012", 
+				cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	
 	}
 }
---
+++ src/main/java/io/github/guisofiati/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package io.github.guisofiati.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.guisofiati.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+}
