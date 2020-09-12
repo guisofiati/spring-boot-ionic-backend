b146ea8e84fdea1a96d74e4601245163473d9162
Sofiati <gui_sf@hotmail.com>
2020-09-12 17:00:56 -0300

    Endpoint - /clientes/{id} -

---
+++ src/main/java/io/github/guisofiati/resources/ClienteResource.java
@@ -0,0 +1,25 @@
+package io.github.guisofiati.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.guisofiati.domain.Cliente;
+import io.github.guisofiati.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {		
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);		
+	}	
+}
