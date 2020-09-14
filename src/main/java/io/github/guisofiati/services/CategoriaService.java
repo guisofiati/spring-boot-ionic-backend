d596a36a473e25f29631966333635bd860af938f
Sofiati <gui_sf@hotmail.com>
2020-09-14 09:44:40 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/io/github/guisofiati/services/CategoriaService.java
+++ src/main/java/io/github/guisofiati/services/CategoriaService.java
@@ -3,10 +3,12 @@ package io.github.guisofiati.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import io.github.guisofiati.domain.Categoria;
 import io.github.guisofiati.repositories.CategoriaRepository;
+import io.github.guisofiati.services.exceptions.DataIntegrityException;
 import io.github.guisofiati.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 }
