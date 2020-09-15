7eb37dec19dee541dc0b82cdcc893e9ce1cc1b42
Sofiati <gui_sf@hotmail.com>
2020-09-15 09:40:15 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/io/github/guisofiati/services/CategoriaService.java
+++ src/main/java/io/github/guisofiati/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import io.github.guisofiati.domain.Categoria;
+import io.github.guisofiati.dto.CategoriaDTO;
 import io.github.guisofiati.repositories.CategoriaRepository;
 import io.github.guisofiati.services.exceptions.DataIntegrityException;
 import io.github.guisofiati.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto ) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
