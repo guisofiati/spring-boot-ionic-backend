943e1d74cc8eb005b7d630aef40d4aa5cbf37242
Sofiati <gui_sf@hotmail.com>
2020-09-18 23:54:17 -0300

    Busca de produtos por nome e categorias

--- src/main/java/io/github/guisofiati/repositories/ProdutoRepository.java
+++ src/main/java/io/github/guisofiati/repositories/ProdutoRepository.java
@@ -1,11 +1,19 @@
 package io.github.guisofiati.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import io.github.guisofiati.domain.Categoria;
 import io.github.guisofiati.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
