7d76536aa5f5b1a54091807d6232b1822eaf83cc
Sofiati <gui_sf@hotmail.com>
2020-09-19 11:29:23 -0300

    Inserindo Pedido

--- src/main/java/io/github/guisofiati/domain/PagamentoComCartao.java
+++ src/main/java/io/github/guisofiati/domain/PagamentoComCartao.java
@@ -2,9 +2,12 @@ package io.github.guisofiati.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import io.github.guisofiati.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
