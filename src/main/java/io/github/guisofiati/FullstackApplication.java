ee7bc382aeb58f8dfdbe4a33b7f0ed835c8b3f42
Sofiati <gui_sf@hotmail.com>
2020-09-27 19:17:24 -0300

    Salvando primeiro arquivo no S3 da Amazon Web Services

--- src/main/java/io/github/guisofiati/FullstackApplication.java
+++ src/main/java/io/github/guisofiati/FullstackApplication.java
@@ -1,17 +1,24 @@
 package io.github.guisofiati;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.guisofiati.services.S3Service;
+
 @SpringBootApplication
 public class FullstackApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {			
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("C:\\Users\\gui_s\\Documents\\sts-projects\\fotos\\11bx.jpg");
 	}
 }
