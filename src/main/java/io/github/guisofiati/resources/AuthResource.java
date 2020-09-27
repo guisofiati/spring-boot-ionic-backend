5685531b49b75dad164994c093f1bea46ed6bf12
Sofiati <gui_sf@hotmail.com>
2020-09-27 00:03:59 -0300

    Esqueci a senha

--- src/main/java/io/github/guisofiati/resources/AuthResource.java
+++ src/main/java/io/github/guisofiati/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package io.github.guisofiati.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.guisofiati.dto.EmailDTO;
 import io.github.guisofiati.security.JWTUtil;
 import io.github.guisofiati.security.UserSS;
+import io.github.guisofiati.services.AuthService;
 import io.github.guisofiati.services.UserService;
 
 @RestController
@@ -19,11 +23,20 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
