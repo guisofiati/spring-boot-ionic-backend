a42261ab67eb6e108f919d37a76f5f8e1bb55828
Sofiati <gui_sf@hotmail.com>
2020-09-26 23:15:18 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/io/github/guisofiati/repositories/PedidoRepository.java
+++ src/main/java/io/github/guisofiati/repositories/PedidoRepository.java
@@ -1,11 +1,17 @@
 package io.github.guisofiati.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import io.github.guisofiati.domain.Cliente;
 import io.github.guisofiati.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/io/github/guisofiati/services/PedidoService.java
+++ src/main/java/io/github/guisofiati/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import io.github.guisofiati.domain.Cliente;
 import io.github.guisofiati.domain.ItemPedido;
 import io.github.guisofiati.domain.PagamentoComBoleto;
 import io.github.guisofiati.domain.Pedido;
@@ -14,6 +18,8 @@ import io.github.guisofiati.domain.enums.EstadoPagamento;
 import io.github.guisofiati.repositories.ItemPedidoRepository;
 import io.github.guisofiati.repositories.PagamentoRepository;
 import io.github.guisofiati.repositories.PedidoRepository;
+import io.github.guisofiati.security.UserSS;
+import io.github.guisofiati.services.exceptions.AuthorizationException;
 import io.github.guisofiati.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -64,5 +70,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;	
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
